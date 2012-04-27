9e6d56bc1b349b327f6bff20fcac3f8210bcfe66
chef2.misc <root@chef2.misc.ord1t>
2012-04-27 10:39:11 -0400

    updated license agreements

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Rackspace, INC"
-maintainer_email "justin@thewitricks.com"
-license          ""
+maintainer       "Justin Witrick"
+maintainer_email "github@thewitricks.com"
+license          "GPL 2"
 description      "DRBD recipe"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
--- recipes/drbd_inplace_upgrade.rb
+++ recipes/drbd_inplace_upgrade.rb
@@ -2,9 +2,22 @@
 # Cookbook Name:: mailserver_provisioning
 # Recipe:: drbd_inplace_upgrade
 #
-# Copyright 2012, RACKSPACE
+# Copyright (C) 2012 Justin Witrick
 #
-# All rights reserved - Do Not Redistribute
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 #
 include_recipe 'drbd'
 stop_file_exists_command = " [ -f #{node[:drbd][:stop_file]} ] "
