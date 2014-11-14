9178bcabf6a1097df6bc79ec11e4b6c24cc3e03c
Justin Witrick <github@thewitricks.com>
2014-11-14 14:32:22 +0000

    Versioned for tagging

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "github@thewitricks.com"
 license          "Apache 2.0"
 description      "DRBD recipe"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.4"
 
 %w{ redhat centos scientific }.each do |os|
   supports os, ">= 5.8"
